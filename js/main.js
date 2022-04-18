429e18fc5f59e819fe3ed4458add5856dc901a99
stepan323446 <stevedekart2020@gmail.com>
2022-04-18 12:14:52 +0200

    .

--- js/main.js
+++ js/main.js
@@ -7,3 +7,12 @@ for(let i = 0; i < dropdown.length; i++)
         dropdown[i].classList.toggle("active");
     });
 }
+
+let burger = document.getElementsByClassName("burger")[0];
+let header = document.getElementsByClassName("header-info")[0];
+
+burger.addEventListener("click", function()
+{
+    burger.classList.toggle("active");
+    header.classList.toggle("active");
+});
